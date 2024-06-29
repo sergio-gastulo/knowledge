44df8d40635d448b684becbf25ad5adc77f405f2
sergio-gastulo <sgastulo04@gmail.com>
2024-06-29 11:56:34 -0500

    nothing new, just ni->New Item

--- powershell_scripts/new_bloc_notas.ps1
+++ powershell_scripts/new_bloc_notas.ps1
@@ -3,18 +3,18 @@ $filename = Read-Host "Enter the file name"
 
 $carpet = Read-Host "ps1 or bloc"
 
-$ps1_path = $('C:\Users\sgast\Documents_personal\powershell_scripts\'+$filename+'.ps1')
+$ps1_path = $('C:\Users\sgast\CODING\powershell_scripts\'+$filename+'.ps1')
 $bloc_path = $('C:\Users\sgast\Documents_personal\bloc_notas\'+$filename+'.txt')
 
 if ($carpet -eq 'ps1') {
 
-	ni $ps1_path
+	New-Item $ps1_path
 	notepad $ps1_path
 
 } elseif ($carpet -eq 'bloc') {
 
 	# Create the file in the Bloc notas folder and open it in Notepad
-	ni $bloc_path
+	New-Item $bloc_path
 	notepad $bloc_path
 
 } else {
