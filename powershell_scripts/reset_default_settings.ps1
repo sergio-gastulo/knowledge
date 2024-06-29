ed3e9d7d9e34f934f22c3e13bbc39e0372aa9606
sergio-gastulo <sgastulo04@gmail.com>
2024-06-29 14:00:10 -0500

    this programs resets Wolfram Mathematica to its default configurations automatically (at least locally on my computer).

---
+++ powershell_scripts/reset_default_settings.ps1
@@ -0,0 +1,10 @@
+Remove-Item C:\ProgramData\Mathematica\ -force
+
+Remove-Item C:\Users\sgast\AppData\Roaming\Mathematica\ -Force
+
+Remove-Item C:\Users\sgast\AppData\Roaming\Wolfram\ -force
+
+Remove-Item C:\Users\sgast\AppData\Local\Wolfram\ -force
+
+Remove-Item C:\Users\sgast\AppData\Local\Mathematica\ -force
+
