f078347df42e155ac2e0733910c0028a6996c06c
sergio-gastulo <sgastulo04@gmail.com>
2024-06-29 11:49:29 -0500

    files which copies and pastes script automatically

---
+++ powershell_scripts/mom_messages.ps1
@@ -0,0 +1,25 @@
+
+while ($True) {
+    $cond1 = "Veci buenas, queremos recordarle que los sabados, domingos y feriados, el plato tiene un precio especial de 13 soles. Esperamos que lo tenga en cuenta para sus siguientes pedidos! (;"
+    $cond2 = "Muchas gracias veci"
+    $cond4 = "El yape es a este numero al nombre de Betty Cespedes"
+    $cond5 = "El plin es al 924182134 al nombre de Sergio Gastulo"
+    "*"*100
+    Write-Host "1 -> " + $cond1
+    Write-Host "2 -> " + $cond2
+    Write-Host "3 -> Pagar pedido"
+    Write-Host "4 -> El yape es a este numero al nombre de Betty Cespedes"
+    Write-Host "5 -> El plin es al 924182134 al nombre de Sergio Gastulo"
+    "*"*100
+    " "
+    $selection = Read-Host "Seleccione su mensaje:"
+    switch ($selection) {
+        1 { $cond1 | Set-Clipboard }
+        2 { $cond2 | Set-Clipboard }
+        3 { 
+            $int = read-host "--"; "Veci buenas no se olvide de yapear por favor, en total es " +$int+" soles" | Set-Clipboard
+        }
+        4 { $cond4 | Set-Clipboard }
+        5 { $cond5 | Set-Clipboard }
+    }
+}
