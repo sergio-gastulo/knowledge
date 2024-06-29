d21cbfedd360df851d5ffbd4c7d23493f501b93b
sergio-gastulo <sgastulo04@gmail.com>
2024-06-29 11:52:40 -0500

    this has been improved to let payments to distribute all over the n following months. However, it still need to take care abaout DateTime datatype since the counters are updated as strings and not as a datetime datatype.

--- powershell_scripts/information_to_cuentas_csv.ps1
+++ powershell_scripts/information_to_cuentas_csv.ps1
@@ -8,44 +8,79 @@
     Getting the day, only the day is necessary because i do this daily, so its literally impossible to fill
     this form from month to month 
 #>
-$day = Read-Host 'Fecha (solo el dia)'
+
 
 #   Getting the date
-$date = Get-Date -Format "dd-MM-yyyy"
-
-#   Formatting the date
-$date = $date -replace "^..", $day
+Write-Host " "
+$day = Read-Host 'Fecha (solo el dia)'
+$month = (Get-Date).Month.ToString("00")
+$year = (Get-Date).Year
+$date = "$day-$month-$year"
 
 #   Register the amount spent
+Write-Host " "
 $monto = READ-host 'Registre el monto ingresado'
 
-#   What kind of spending the amount was?
-$wordList = @("CELULAR", "CASA", "MENU", "PASAJE", "BLIND", "INGRESO","VARIOS", "COM_VAR","PERSONAL","RECIBO")
+#category list
+$wordList = @("BLIND", "CASA", "CELULAR", "COM_VAR", "INGRESO", "MENU", "PASAJE", "PERSONAL", "RECIBO", "USD_INC", "VARIOS")
+
+Write-Host " "
+"*"*100
+Write-Host " "
+#   Displaying the categories with numbers
+Write-Host "Seleccione la categoria correspondiente:"
+for ($i = 0; $i -lt $wordList.Length; $i++) {
+    Write-Host "$($i + 1) -> $($wordList[$i])"
+}
+Write-Host " "
+"*"*100
+Write-Host " "
 
 #   Selecting the kind of spending the amount was
-$selectedWord = Read-Host "Seleccione la categoria correspondiente: $($wordList -join ', ')"
+$selectedNumber = Read-Host "Type category number"
 
 #   Validating the input
-while ($selectedWord -notin $wordList) {
-    Write-Host "Invalid input. Please select a word from the list."
-    $selectedWord = Read-Host "Seleccione la categoria correspondiente: $($wordList -join ', ')"
+while (-not ($selectedNumber -match '^\d+$') -or [int]$selectedNumber -le 0 -or [int]$selectedNumber -gt $wordList.Length) {
+    $selectedNumber = Read-Host "Type valid category number"
 }
 
-#   The description of the amount spent, so everything is properly tracked
-$descripcion = READ-HOST 'Poner breve descripcion'
+#   Getting the selected word
+$selectedWord = $wordList[[int]$selectedNumber - 1].Trim()
 
-#   The row to be append on the csv file
-$value = "$date,$monto,$descripcion,$selectedWord"
+# Solicitar una breve descripción al usuario
+Write-Host " "
+$descripcion = Read-Host "type descripcion -- no commas no ENHE "
+Write-Host " "
+
 
 #   The path of the csv file
 $paths = 'C:\Users\sgast\Documents_personal\excel\cuentas.csv'
 
-#   Appending the row to the csv file
-add-content -path $paths -value "`n$value"
+if (([int]$monto -gt 200) -and (($selectedWord -ne "INGRESO") -and ($selectedWord -ne "USD_INC"))) {
+    
+    <# Action to perform if the condition is true #>
+    $numMonths = Read-Host "How many months do you want to your value into your csv"
+    
+    $newMonto = ([int]$monto)/([int]$numMonths)
+    
+    for ($i = 0; $i -lt $numMonths; $i++) {
+        
+        $newMonth = ([int]$Month+$i).ToString("00")
+        $newDate = "$day-$newMonth-$year"        
+        $newDesc = "$descripcion tag: cuota $i"
+        #   The row to be append on the csv file
+        $value = "$newDate,$newMonto,$newDesc,$selectedWord"
+        
 
+        add-content -path $paths -value "$value"
+    }
+} else {
+    <# Action when all if and elseif conditions are false #>
+    
+    $value = "$date,$monto,$descripcion,$selectedWord"
 
-
-
-
-
-
+    <# 
+    Just appending the row to the csv file, withouth anything changed 
+    #>
+    add-content -path $paths -value "$value"
+}
