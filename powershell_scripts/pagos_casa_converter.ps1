e976b18adbd317ea6bdc486909b665742e34c417
sergio-gastulo <sgastulo04@gmail.com>
2024-06-29 14:13:00 -0500

    Auto-commit: 2024-06-29 14:13:00

--- powershell_scripts/pagos_casa_converter.ps1
+++ powershell_scripts/pagos_casa_converter.ps1
@@ -5,7 +5,9 @@
 $download_path = "C:\Users\sgast\Downloads\"
 
 #every png file name is replaced with no spaces
-ls $download_path | % { rni $_.Fullname -newname ($_.Name -replace ' ', '_') }
+Get-ChildItem $download_path | ForEach-Object { 
+	Rename-Item $_.Fullname -newname ($_.Name -replace ' ', '_') 
+}
 
 #the month we are currently paying
 $path = "C:\Users\sgast\Documents_personal\excel\casa_accounting\" + (Read-Host "give the name of the month youre paying") + "\"
@@ -15,12 +17,12 @@ if (-not (test-path -Path $path)){
 }
 
 #moving each wsp png file to the desired carpet 
-ls $download_path -Filter 'Wha*'| % {mv $_.FullName -Destination $path}
+Get-ChildItem $download_path -Filter 'Wha*'| ForEach-Object {Move-Item $_.FullName -Destination $path}
 
 #it opens each file and then asks for its coresponding name with png already
-ls $path -Filter 'Wha*' | % {
-	saps -FilePath "msedge.exe" -ArgumentList $_.FullName; 
-	rni $_.FullName -NewName ((Read-Host "Name of the payment") + ".png")
+Get-ChildItem $path -Filter 'Wha*' | ForEach-Object {
+	Start-Process -FilePath "msedge.exe" -ArgumentList $_.FullName; 
+	Rename-Item $_.FullName -NewName ((Read-Host "Name of the payment") + ".png")
 }
 
 
