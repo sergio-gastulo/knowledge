0a4c527859f1008aa0e108402f103a3a3864be73
sergio-gastulo <sgastulo04@gmail.com>
2024-07-13 15:59:49 -0500

    Commit message updated, now it can be inputed

--- bash_for_git/profile_to_git.sh
+++ bash_for_git/profile_to_git.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 profile_path="/c/Users/sgast/OneDrive/Documentos/WindowsPowerShell/Microsoft.PowerShell_profile.ps1"
+
 destination_path="/c/Users/sgast/CODING/powershell_scripts/"
 
 cp "$profile_path" "$destination_path"
 
-git add "/powershell_scripts/Microsoft.PowerShell_profile.ps1"
+git add "powershell_scripts/Microsoft.PowerShell_profile.ps1" 
 
-git commit -m "Auto-commit: $(date '+%Y-%m-%d')"
+echo "Enter the commit message:"
+
+read commit_message
+
+git commit -m "$commit_message"
 
 git push -u origin main
