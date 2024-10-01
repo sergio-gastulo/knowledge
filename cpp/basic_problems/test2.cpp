d66cd82b431232594e5eada9d250ecee657cff9e
sergio-gastulo <sgastulo04@gmail.com>
2024-09-30 21:36:21 -0500

    Auto-commit: 2024-09-30 21:36:20

--- cpp/basic_problems/test2.cpp
+++ cpp/basic_problems/test2.cpp
@@ -1,10 +1,22 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
+#include<string>
+
 using namespace std;
 
 int main(){
-    for( int i = 0; i<6; i++) {
-        cout<<"Hello World!"<<endl;
-    }
+
+    cout << "Find size of fundamental datatypes" << endl;
+    cout << "----------------------------------" << endl;
+    cout << "the size of char is "<< sizeof(char) << " bytes." << endl;
+    cout << "the size of short is "<< sizeof(short) << " bytes." << endl;
+    cout << "the size of int is "<< sizeof(int) << " bytes." << endl;
+    cout << "the size of long is "<< sizeof(long) << " bytes." << endl;
+    cout << "the size of long long is "<< sizeof(long long ) << " bytes." << endl;
+    cout << "the size of float is "<< sizeof(float) << " bytes." << endl;
+    cout << "the size of double is "<< sizeof(double) << " bytes." << endl;
+    cout << "the size of long double is "<< sizeof(long double ) << " bytes." << endl;
+    cout << "the size of bool is "<< sizeof(bool) << " bytes." << endl;
 
     return 0;
 }
