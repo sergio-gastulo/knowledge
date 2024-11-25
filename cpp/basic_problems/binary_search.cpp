4911ca887b14feb5eebe6b1fea390444f3ec05cf
sergio-gastulo <sgastulo04@gmail.com>
2024-11-24 19:16:42 -0500

    basic stuf

---
+++ cpp/basic_problems/binary_search.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void binary_search(){
+    cout << "hello world!"<< endl;
+}
+
+int main(){
+    string my_var; 
+    cout << "ingresa tu nombre" << endl;
+    cin >> my_var;
+    cout << "que chevere te llamas" << my_var << endl;
+
+    binary_search();
+    return 0;
+}
